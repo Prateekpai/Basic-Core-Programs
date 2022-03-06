8b48b19970788dd6b55294cae33e3e5d8f5f2404
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 18:00:00 +0530

    Swap Two Numbers

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwapNumbers
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter first Number : ");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second number :");
+            b =Convert.ToInt32(Console.ReadLine());
+
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After Swaping: ");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
