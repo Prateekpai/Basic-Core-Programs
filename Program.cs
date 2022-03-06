896013a569db5e3ea35d53e8a6edca24e4be6f0a
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 17:38:45 +0530

    Compute Quotient and Remainder

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace QuotientAndReminder
+{
+    public class Program
+    {
+        public static void Main (string[]args)
+        {
+            int a,b;
+
+            Console.WriteLine("Enter a Dividend: ");
+            a =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a Divisor: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int Quotient = a / b;
+            int reminder = b / a;
+            Console.WriteLine( "The Quotient is :{0}",Quotient);
+            Console.WriteLine("The Remainder is : {0}",reminder);
+        }
+    }
+}
