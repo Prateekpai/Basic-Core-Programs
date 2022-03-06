97c719032da269e831fbccfb6e6aa7a79e36e1fe
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 17:56:30 +0530

    Prime Factorization

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Prime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Please Enter integer: ");
+
+            a = int.Parse(Console.ReadLine());
+
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
+        }
+    }
+}
