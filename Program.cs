ed27e94d26bce0229b3664c4d7818b7e0b086ab2
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 17:43:02 +0530

    Even or Odd

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvenOrOdd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            int a;
+
+            Console.WriteLine("Enter a Number: ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            {
+
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Entered Number is Even");
+                }
+                else
+                {
+                    Console.WriteLine("Enter Number is Odd");
+                }
+            }
+        }
+    }
+}
