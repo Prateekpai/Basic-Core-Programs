901ee6e9722c7d73576300c61e0e23d9c3667d7e
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 18:02:12 +0530

    Vowel or Consonant

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace VowelOrConsonant
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Char alp;
+            Console.WriteLine("Enter a Alphabet");
+            alp = Convert.ToChar(Console.ReadLine());
+
+            {
+                if (alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u' || alp == 'A' || alp == 'E' || alp == 'I' || alp == 'O' || alp == 'U')
+                {
+                    Console.WriteLine("The {0} Alphabet is Vowel:", alp);
+                }
+
+                else
+                {
+                    Console.WriteLine("The {0} Alphabet is Consonant:", alp);
+                }
+            }
+        }
+    }
+}
