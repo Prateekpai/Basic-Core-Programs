794aab88090a7dd031f3e6ef9b150686c0180404
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 17:34:13 +0530

    WelCome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Welcome
+{
+    public class program
+    {
+        public static void Main(string[]args)
+
+        {
+            Console.WriteLine("Welcome to Day 5 Practice Problem");
+        }
+    }
+}
