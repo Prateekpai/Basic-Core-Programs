6a48f86b3205a4ddcd732cb80effbd20346229e0
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-06 17:46:34 +0530

    Harmonic Number

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace HarmonicNumberProgram
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int i, n;
+            {
+                Console.Write("Input the number of terms : ");
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+
+            }
+        }
+    }
+}
+
